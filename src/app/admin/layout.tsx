5de37ccea8f9a0edb35d432f0a1190b664e7a723
TranVanLuong <tranvanluong032020@gmail.com>
2025-11-20 21:36:02 +0700

    up

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,20 +1,14 @@
 import { AppSidebar } from "@/components/admin/AppSidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { TooltipProvider } from "@/components/ui/tooltip";
-export default function AdminLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <SidebarProvider>
         <TooltipProvider>
           <div className="flex min-h-screen w-full">
             <AppSidebar />
-            <SidebarInset className="flex-1 w-full min-w-0">
-              {children}
-            </SidebarInset>
+            <SidebarInset className="flex-1 w-full min-w-0">{children}</SidebarInset>
           </div>
         </TooltipProvider>
       </SidebarProvider>
