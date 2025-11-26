60c5fb2bb6d91a69ae7337eb35f2ea43f45cad9d
TranVanLuong <tranvanluong032020@gmail.com>
2025-11-26 21:29:33 +0700

    done

--- src/services/upload.service.ts
+++ src/services/upload.service.ts
@@ -1,12 +1,12 @@
-import publicAxios from "@/lib/axios/publicAxios";
+import privateAxios from "@/lib/axios/privateAxios";
 import { IBackendRes } from "@/types/backend.type";
 
 const UploadService = {
-  uploadFile: (file: File): Promise<IBackendRes<{ url: string, createdAt: string }>> => {
+  uploadFile: (file: File): Promise<IBackendRes<{ url: string; createdAt: string }>> => {
     const formData = new FormData();
     formData.append("file", file);
 
-    return publicAxios.post(`/file/upload`, formData, {
+    return privateAxios.post(`/file/upload`, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
